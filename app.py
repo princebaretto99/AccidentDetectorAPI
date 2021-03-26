5bb1c773b60a3d0636c500977a0528c3da11e6a0
princebaretto99 <princebaretto@gmail.com>
2021-03-26 20:08:26 +0530

    pushed new content

--- app.py
+++ app.py
@@ -5,15 +5,30 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.preprocessing import image
+import uuid
+import os
+    
+
+import urllib.request
 
 app = Flask(__name__)
+
+def url_to_jpg(uri,file_path):
+    urllib.request.urlretrieve(uri,file_path)
 
 @app.route('/', methods = ["GET"])
 def predict():
     # load model
     model = load_model('./models/epochs20.h5')
+    uri = "https://firebasestorage.googleapis.com/v0/b/pacman-73ff5.appspot.com/o/events%2Fa%40g.com%2Fimage0.279252745985788751234?alt=media&token=eed00af5-5563-4458-91bd-a46e30a9fbe5"
     
-    img_path = "./images/no.jpg"
+    id = str(uuid.uuid1())
+    filename = id+".jpg"
+    file_path = "images/" + filename
+    url_to_jpg(uri,file_path)
+
+    # print("file Saved")
+    img_path = file_path
     img = image.load_img(img_path, target_size=(224, 224))
 
     img_array = image.img_to_array(img)
@@ -39,8 +54,11 @@ def predict():
                 200,
             )
     response.headers["Content-Type"] = "application/json"
+    os.remove(file_path)
+    # print("file deleted")
 
     return response
+
 
 if __name__ == '__main__':
     app.run()
